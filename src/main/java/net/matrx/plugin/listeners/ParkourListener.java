8d6d1f8567d4410cd3f295311f9b6f470394a935
jackmarkas9-max <jackmarkas9@gmail.com>
2026-06-17 10:37:17 +0300

    Fix missing imports in ParkourListener

--- src/main/java/net/matrx/plugin/listeners/ParkourListener.java
+++ src/main/java/net/matrx/plugin/listeners/ParkourListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
+import java.util.*;
 
 public class ParkourListener implements Listener {
     private final CourseManager courseManager;
